d43c86beda806985444a3dd1cef44a8bc1b2f3d9
Stefan Antoci <stefan.antoci@sabs.ro>
2020-12-01 18:39:45 +0200

    add cpu result

--- Pi_estimation_gpu/Pi_estimation_cpu/Pi_estimation_cpu.cpp
+++ Pi_estimation_gpu/Pi_estimation_cpu/Pi_estimation_cpu.cpp
@@ -19,7 +19,7 @@ int main()
 		while (temp < m) {
 			float x = distribution(generator);
 			float y = distribution(generator);
-			float r = x * x + y * y; 
+			float r = x * x + y * y;
 
 			if (r <= 1) {
 				count++;
@@ -41,7 +41,8 @@ int main()
 	}
 	clock_t cpu_stop = clock();
 	std::cout << "Approximate pi calculated on CPU is: " << pi << " and calculation took " << (double)(cpu_stop - cpu_start) / CLOCKS_PER_SEC << std::endl;
-
+	int age;
+	cin >> age;
 
 
 }
