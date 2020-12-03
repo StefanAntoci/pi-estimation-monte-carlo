d9f705ba4220720e562e39e29f03ed2c7fa54668
Stefan Antoci <stefan.antoci@sabs.ro>
2020-12-04 00:44:56 +0200

    change gpu algo

--- pi_est_gpu_naive.cu
+++ pi_est_gpu_naive.cu
@@ -1,93 +1,83 @@
-#define INTERVAL 512*512*128
+#define INTERVAL 16777216
+#define ITEMS_IN_THREAD 16
+#define THREADS_PER_BLOCK 16
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 #include <random>
 
 using namespace std;
-__global__ void count_pi_1(float *dev_randX, float *dev_randY, int *dev_threads_num, int n) {
-	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	int cont = 0;
-	for (int i = tid * 128; i < 128 * (tid + 1); i++) {
-		if (dev_randX[i] * dev_randX[i] + dev_randY[i] * dev_randY[i] < 1.0f) {
-			cont++;
-		}
-	}
-	dev_threads_num[tid] = cont;
-}
+__global__ void count_pi_1( float *dev_randX, float *dev_randY, int *dev_threads_num ) {
+	int tid =  blockIdx.x * blockDim.x  + threadIdx.x ;
+	float result = dev_randX[tid] * dev_randX[tid] + dev_randY[tid] * dev_randY[tid];
+  if (result <= 1.0) {
+      dev_threads_num[tid] = 1;
+  } else {
+      dev_threads_num[tid] = 0;
+  }
+}	
 
 int main()
-{
-  cout << "entry point" << flush << endl;
- clock_t c_start, c_end;
- float* randX;
- float* randY;
- 	size_t size = INTERVAL * sizeof(float);
- randX = (float*) malloc (size);
- randY = (float*) malloc (size);
-
- srand((unsigned)time(NULL));
- 
-  
+{ 
+  vector<float> randX(INTERVAL);
+	vector<float> randY(INTERVAL);
+	clock_t c_start, c_end;
+	srand((unsigned)time(NULL));
 	for (int i = 0; i < INTERVAL; i++) {
 		randX[i] = float(rand()) / RAND_MAX;
 		randY[i] = float(rand()) / RAND_MAX;
 	}
- 
+  //cout << "initial array" << endl << endl;
+  //for (int i = 0; i < INTERVAL; i++) {
+	//	cout << randX[i] << " | ";
+	//}
 
-	float pi;
- 
+	//start cont gpu time
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+	cudaEventRecord(start, 0);
 
- // set up timing stuff
-	float gpu_elapsed_time;
-	cudaEvent_t gpu_start, gpu_stop;
-	cudaEventCreate(&gpu_start);
-	cudaEventCreate(&gpu_stop);
-	cudaEventRecord(gpu_start, 0);
-
-	// allocate memory
-
+	//send data to GPU
+	size_t size = INTERVAL * sizeof(float);
 	float *dev_randX;
 	float *dev_randY;
 	cudaMalloc((void**)&dev_randX, size);
 	cudaMalloc((void**)&dev_randY, size);
-  cudaMemcpy(dev_randX, randX, size, cudaMemcpyHostToDevice);
-	cudaMemcpy(dev_randY, randY, size, cudaMemcpyHostToDevice);
- cout << "memory allocation" << flush << endl;
+
+	cudaMemcpy(dev_randX, &randX.front(), size, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_randY, &randY.front(), size, cudaMemcpyHostToDevice);
+
+	
+	int block_num = INTERVAL / THREADS_PER_BLOCK ;
 	int *dev_threads_num;
-  //block dimensions setup
-  int threadsPerBlock = 512;
-	int block_num = INTERVAL / (128 * threadsPerBlock);
-	cudaMalloc((void**)&dev_threads_num, INTERVAL / 128 * sizeof(int));
-
+	cudaMalloc((void**)&dev_threads_num,  INTERVAL  * sizeof(int) );
  
-	count_pi_1 <<<block_num, threadsPerBlock >>> (dev_randX, dev_randY, dev_threads_num,INTERVAL);
+ 	float *test;
+	cudaMalloc((void**)&test,  INTERVAL  * sizeof(float) );
 
-	int* threads_num = new int[INTERVAL / 128];
-	cudaMemcpy(threads_num, dev_threads_num, INTERVAL / 128 * sizeof(int), cudaMemcpyDeviceToHost);
+	count_pi_1 <<<block_num, THREADS_PER_BLOCK >>> ( dev_randX, dev_randY, dev_threads_num );
+
+	int* threads_num = (int*)malloc( INTERVAL * sizeof(int) );
+	cudaMemcpy(threads_num, dev_threads_num, INTERVAL * sizeof(int), cudaMemcpyDeviceToHost);
+
+	float* test_host = (float*)malloc( INTERVAL * sizeof(float) );
+	cudaMemcpy(test_host, test, INTERVAL * sizeof(float), cudaMemcpyDeviceToHost);
 
 	int g_count = 0;
-	for (int i = 0; i < INTERVAL / 128; i++) {
+	for (int i = 0; i < INTERVAL ; i++) {
 		g_count += threads_num[i];
 	};
 
 	//end cont gpu time
-	cudaEventRecord(gpu_stop, 0);
-	cudaEventSynchronize(gpu_stop);
+	cudaEventRecord(stop, 0);
+	cudaEventSynchronize(stop);
 	float t_gpu1;
-	cudaEventElapsedTime(&t_gpu1, gpu_start, gpu_stop);
-	cudaEventDestroy(gpu_start);
-	cudaEventDestroy(gpu_stop);
-
+	cudaEventElapsedTime(&t_gpu1, start, stop);
+	cudaEventDestroy(start);
+	cudaEventDestroy(stop);
 	float g_num = float(g_count) * 4.0 / INTERVAL;
 	cout << "GPU_1 Time" << endl;
 	cout << g_num << endl;
 	cout << "time = " << t_gpu1 << " ms" << endl;
-
-	// delete memory
-	cudaFree(dev_randX);
-	cudaFree(dev_randY);
- 	cudaFree(dev_threads_num);
- 
- return 0; 
 }
